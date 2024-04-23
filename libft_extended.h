e273e61e260f8bf5f1d4c5362717c92234c33de5
Bryan-da-silvaa <97440568+Bryan-da-silvaa@users.noreply.github.com>
2024-04-23 09:15:57 +0000

    (ADD) Header file libft_extended.h

---
+++ libft_extended.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_EXTENDED_H
+# define LIBFT_EXTENDED_H
+
+typedef struct         s_list
+{
+    void               *content;
+    struct s_list      *next;
+}                      t_list;
+
+#endif
